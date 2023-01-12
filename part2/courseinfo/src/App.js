96109b6a007454ddcf5844c5d8002ab01964b3d3
deansensei <d.tung.dinh@gmail.com>
2023-01-12 22:08:17 +0100

    Exercise 2.3

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+const Header = ({ course }) => <h1>{course}</h1>
 
-function App() {
+const Total = ({ sum }) => <p><strong>total of exercises {sum}</strong></p>
+
+const Course = (props) => {
+const initialValue = 0;
+const sumWithInitial = props.course.parts.reduce(
+  (accumulator, currentValue) => accumulator + currentValue.exercises, initialValue)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+      <Total sum={sumWithInitial} />
     </div>
-  );
+    )
 }
 
-export default App;
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = (props) => {
+  return(
+    <>
+    {props.parts.map(part => <Part key={part.id} part={part} />)}
+    </>
+  )
+
+}
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
