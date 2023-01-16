4a8581d1c572b3ee3b13bdab7de5f621c3c2ef77
deansensei <d.tung.dinh@gmail.com>
2023-01-16 20:56:01 +0100

    Exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -31,43 +31,46 @@ function areTheseObjectsEqual(first, second) {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' , number : '90121 212'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   
   const addContact = (event) => {
     event.preventDefault()
-    const found = persons.find(element => areTheseObjectsEqual({name : newName}, element))
+    const newObj = {name : newName, number : newNumber}
+    const found = persons.find(element => areTheseObjectsEqual(newObj, element))
 
     if (found) {
       window.alert(`${newName} is already added to phonebook`);
     }
     else {
-      setPersons(persons.concat({name : newName}))
+      setPersons(persons.concat(newObj))
       setNewName('')
     }
 
  
   }
   const handleNameChange = (event) => {
-    
     setNewName(event.target.value)
+  }
+
+  const handleNameNumber = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          <button type="submit" onClick={addContact}>add</button>
-        </div>
-      </form>
+
+    <form>
+      <div>name: <input value={newName} onChange={handleNameChange} /></div>
+      <div>number: <input value={newNumber} onChange={handleNameNumber} /></div>
+      <div><button type="submit" onClick={addContact}>add</button></div>
+    </form>
 
       <h2>Numbers</h2>
-      <div>{persons.map(person => <div key={person.name}>{person.name}</div>)}</div>
+      <div>{persons.map(person => <div key={person.name}>{`${person.name} ${person.number}`}</div>)}</div>
     </div>
     
   )
