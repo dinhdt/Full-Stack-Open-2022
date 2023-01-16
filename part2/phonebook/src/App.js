8c06e386d64e9479f526cbb266bf4a09b9247381
deansensei <d.tung.dinh@gmail.com>
2023-01-16 20:43:21 +0100

    Exercise 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,34 @@
 import { useState } from 'react'
 
+
+function areTheseObjectsEqual(first, second) {
+  const al = Object.getOwnPropertyNames(first);
+  const bl = Object.getOwnPropertyNames(second);
+
+  // Check if the two list of keys are the same
+  // length. If they are not, we know the objects
+  // are not equal.
+  if (al.length !== bl.length) return false;
+
+  // Check that all keys from both objects match
+  // are present on both objects.
+  const hasAllKeys = al.every(value => !!bl.find(v => v === value));
+
+  // If not all the keys match, we know the
+  // objects are not equal.
+  if (!hasAllKeys) return false;
+
+  // We can now check that the value of each
+  // key matches its corresponding key in the
+  // other object.
+  for (const key of al) if (first[key] !== second[key]) return false;
+
+  // If the object hasn't return yet, at this
+  // point we know that the objects are the
+  // same
+  return true;
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
@@ -8,10 +37,20 @@ const App = () => {
   
   const addContact = (event) => {
     event.preventDefault()
-    setPersons(persons.concat({name : newName}))
-    setNewName('')
+    const found = persons.find(element => areTheseObjectsEqual({name : newName}, element))
+
+    if (found) {
+      window.alert(`${newName} is already added to phonebook`);
+    }
+    else {
+      setPersons(persons.concat({name : newName}))
+      setNewName('')
+    }
+
+ 
   }
   const handleNameChange = (event) => {
+    
     setNewName(event.target.value)
   }
 
