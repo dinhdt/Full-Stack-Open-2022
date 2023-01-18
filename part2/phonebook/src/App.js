08bf1e13ac39f190db359990423a07e850878405
deansensei <d.tung.dinh@gmail.com>
2023-01-18 20:30:15 +0100

    Exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -31,10 +31,15 @@ function areTheseObjectsEqual(first, second) {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' , number : '90121 212'}
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
   
   const addContact = (event) => {
     event.preventDefault()
@@ -59,10 +64,17 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const namesToShow = persons.filter(person => person.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
-
+      <div>filter shown with <input value={filter} onChange={handleFilter} /></div>
+      <h2>add a new</h2>
     <form>
       <div>name: <input value={newName} onChange={handleNameChange} /></div>
       <div>number: <input value={newNumber} onChange={handleNameNumber} /></div>
@@ -70,7 +82,7 @@ const App = () => {
     </form>
 
       <h2>Numbers</h2>
-      <div>{persons.map(person => <div key={person.name}>{`${person.name} ${person.number}`}</div>)}</div>
+      <div>{namesToShow.map(person => <div key={person.name}>{`${person.name} ${person.number}`}</div>)}</div>
     </div>
     
   )
