eaf57abb91a7730fe8557dd0b3d46f23949633b1
deansensei <d.tung.dinh@gmail.com>
2023-05-29 22:09:42 +0200

    4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,8 +6,22 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, item) => item.likes + sum, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((maxObj, item) => {
+        if (item) {
+            if(maxObj && item.likes < maxObj.likes) {
+                return maxObj
+            }
+            else {
+                return { title : item.title, author : item.author, likes : item.likes }
+            }
+        }
+        return maxObj
+    }, {})
+}
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
