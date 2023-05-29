e037b947f2d991ca6d64bd677dd6c64bfe9f7c04
deansensei <d.tung.dinh@gmail.com>
2023-05-29 21:42:55 +0200

    4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, item) => item.likes + sum, 0)
+}
+
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
